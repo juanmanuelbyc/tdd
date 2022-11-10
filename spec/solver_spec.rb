6f09df964af7e71822759abb8608c988a50d8edf
Juan Diaz <jfdiaza@unal.edu.co>
2022-11-10 04:49:01 -0500

    create test for reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,4 +7,9 @@ describe Solver do
   it 'Factorial of 5 must equal 120' do
     expect(@solver.factorial(5)).to be 120
   end
+
+  it 'reverse of hello must equal olleh' do
+    expect(@solver.reverse('hello')).to be 'olleh'
+  end
+
 end
