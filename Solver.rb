69f3b659d10bd9deed84204d3fa3316847ce77f9
Juan Diaz <jfdiaza@unal.edu.co>
2022-11-10 04:46:29 -0500

    create factorial method

--- Solver.rb
+++ Solver.rb
@@ -1,10 +1,10 @@
 class Solver
-    def factorial(n)
-        raise 'Invalid number!' unless n>=0
-        fact = 1
-        for i in 1..n do
-            fact = fact * i
-        end
-        fact
+  def factorial(n)
+    raise 'Invalid number!' unless n>=0
+    fact = 1
+    for i in 1..n do
+      fact = fact * i
     end
+    fact
+  end
 end
