86a954a2a6087c861d03260325d0bbb044110faf
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-11-10 14:06:13 +0100

    Fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,25 +1,32 @@
+# frozen_string_literal: true
+
+# rubocop:disable Style/Documentation
+
 class Solver
-  def factorial(n)
-    raise 'Invalid number!' unless n>=0
+  def factorial(num)
+    raise 'Invalid number!' unless num >= 0
+
     fact = 1
-    for i in 1..n do
-      fact = fact * i
+    (1..num).each do |i|
+      fact *= i
     end
     fact
   end
+
   def reverse(word)
     word.reverse
   end
 
-  def fizzbuzz (n)
-   if n % 3 == 0 && n % 5 == 0
-    return "fizzbuzz"
-   elsif n % 3 == 0
-    return "fizz"
-   elsif n % 5 == 0
-    return "buzz"
-   else
-    return n.to_s
-   end
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
   end
 end
+# rubocop:enable Style/Documentation
